e0c7dd7e42f0197d59849fdb941f2f5e1d59252d
Ceshine Lee <shuanck@gmail.com>
2020-10-02 14:29:48 +0800

    Update front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Random Ramblings on the Spectrum<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@ceshine_en</span>
+          <h2>A blog for me to rant and be a judgemental jerk.</h2>
           <SocialList />
         </div>
       </div>
